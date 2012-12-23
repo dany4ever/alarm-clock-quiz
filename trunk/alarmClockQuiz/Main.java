48c9038c89ca305550e2594c72478d7eaa7545fa
micah.gale@gmail.com <micah.gale@gmail.com@9b538536-2154-d83f-8781-f2c13c35e175>
2012-12-23 22:20:46 +0000

    Worked on xml parser

--- trunk/alarmClockQuiz/Main.java
+++ trunk/alarmClockQuiz/Main.java
@@ -19,6 +19,16 @@
  */
 package alarmClockQuiz;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**The class that will be run and actually do the things
  *
  * @author Rocky Mountain Technical student Association (Cesar Cortez, Micah Gale micah.gale@gmail.com, Brad Whitesell) 
@@ -39,7 +49,38 @@ public class Main {
      * @return an array of the questions that were parsed.
      */
     public question[] parseQuizXML(String URL) {
-        question[] output = new question[0];
+        BufferedReader reader;
+         String input="";
+        try {                                    //try to read the file
+            reader= new BufferedReader(new FileReader(URL));
+            String buffer;
+            while((buffer=reader.readLine())!=null) {
+                input = input+buffer;                    //combine the whole file to a single string
+            }
+            reader.close();                         //don't leak resources!!!!!!!!!
+        } catch (FileNotFoundException ex ) {      //TODO create better logger
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+        }       
+        ArrayList<question> storage = new ArrayList<>();
+        input=input.replaceAll("<!--\\p{Print}*?-->","");              //take out all comments first
+        Pattern pattern = Pattern.compile("<quiz>\\p{Print}+?</quiz>");  //match <quiz>as little other stuff</quiz>
+        Matcher matcher= pattern.matcher(input);                          //compare it
+        if(matcher.find()) {                         //if found stuff
+            input=matcher.group();             //get the quiz subsection
+            pattern = Pattern.compile("<question>\\p{Print}+?</question>");   //pattern to get question tags
+            matcher = pattern.matcher(input);
+            while(matcher.find()) {
+                String subInput=matcher.group();              //get the found string
+                pattern = Pattern.compile("<answer\\s+correct=\"true\"\\s*>\\p{Print}+?</answer>");  //find the correct answer
+                Matcher correct=pattern.matcher(subInput);
+                pattern =Pattern.compile("<answer\\s+(correct=\"false\")?\\s*>\\p{Print}+?</answer>");  //find incorrect answer
+                Matcher wrong=pattern.matcher(subInput);
+                
+            }
+        }
+        question[] output=new question[storage.size()];
         return output;
     }
 }
