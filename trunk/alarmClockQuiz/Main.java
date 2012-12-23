f810339645ab729628e73a93e34f0d68641f1f52
micah.gale@gmail.com <micah.gale@gmail.com@9b538536-2154-d83f-8781-f2c13c35e175>
2012-12-23 01:47:34 +0000

    Created main.java and an example of the xml file.

---
+++ trunk/alarmClockQuiz/Main.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 Rocky Mountain High School Technical Student Association
+ *
+ * This file is a part of alarm-clock-quiz
+ *
+ * alarm-clock-quiz is free software licensed under the GNU General Public License version 3.
+ * You may redistribute and/or modify it under the terms of the GNU General Public License
+ * version 3 as published by the Free Software Foundation.updater
+ *
+ * alarm-clock-quiz comes without a warranty; without even the implied warranty of merchantability
+ * or fitness for a particular purpose. See the GNU General Public License version 3 for more
+ * details.
+ *
+ * You should have received the GNU General Public License version 3 with this in GPL.txt
+ * if not it is available at <http://www.gnu.org/licenses/gpl.txt>.
+ *
+ * All resources for alarm-clock-quiz including: source code, compiled jar, documentation, and source
+ * notes are available at <https://code.google.com/p/alarm-clock-quiz/>.
+ */
+package alarmClockQuiz;
+
+/**The class that will be run and actually do the things
+ *
+ * @author Rocky Mountain Technical student Association (Cesar Cortez, Micah Gale micah.gale@gmail.com, Brad Whitesell) 
+ * @version 0.10
+ * @since 22 December 2012
+ */
+public class Main {
+    /**The start function will pass off onto others
+     * 
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    /**Parses the XML file as questions and returns an array of the parsed information.
+     * 
+     * @param URL  the URL to the XML file
+     * @return an array of the questions that were parsed.
+     */
+    public question[] parseQuizXML(String URL) {
+        question[] output = new question[0];
+        return output;
+    }
+}
